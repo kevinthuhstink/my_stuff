1495fc55a5ecdca41d7add6ec9b28159b14587cf
kevinthuhstink <kev72eat@gmail.com>
2023-09-14 14:59:07 -0400

    lab 00, 01

--- personal/leetcode/stack/largest_hist.java
+++ personal/leetcode/stack/largest_hist.java
@@ -29,10 +29,9 @@ and make sure the constraint in Observation 2 holds.
 
 Observation 4 (eliminate left scan):
 Lets say we've been using a list L to store indices to consider them out of order to avoid rescanning.
-When we consider L[h_len-1] we know that the element before it (L[h_len-2]) could be equal or smaller to it.
+When we consider L[L.length-1] we know that the element before it (L[L.length-2]) could be equal or smaller to it.
 If it is strictly smaller, we know the left smaller element and can calculate the area which would be the max using a[i-1] with the brute force formula.
-If it is equal, it means after removing L[L.Length-1],
-the constraint in observation 2 still holds (a[L[Length-2]] > a[i]).
+If it is equal, it means after removing L[L.Length-1], the constraint in observation 2 still holds (a[L[Length-2]] > a[i]).
 So for L[L.Length-2] we still know the right and left smaller and can calculate area which would include L[L.Length-1].
 This means it is ok to consider L[L.Length-2] as the left min while considering L[L.Length-1] while maintain right side constraint,
 and the left side falls into place (if equal, in the next iteration, when we consider L[L.Length-2]).
@@ -50,11 +49,63 @@ and a[i] must be smaller than it to avoid right scan (a[L[L.Length-1]] > a[i]).
 We can always consider stack[top-1] to be the left min to avoid left scan, and the max area will fall into place eventually.
 At any point stack[top] is the index we are considering with a[i] the right min and stack[top-1] the left min.
 */
-
 public class largest_hist {
 
     public int solve( int[] heights ) {
-        return 0;
+        Stack<Integer> indices = new Stack<>();
+
+        //0. load the first item into the stack
+        indices.push( 0 );
+        int area = 0;
+        int try_area;
+
+        //1. find a "slice" of the histogram 
+        //   a group of increasing heights looks like a pizza slice
+        for ( int i = 1; i < heights.length; i++ ) {
+            //1a. if ( a[ i - 1 ] > a[ i ] ) the slice is dead
+            if ( heights[ i - 1 ] > heights[ i ] ) {
+                
+                //2. when the slice dies run through the slice for their areas
+                while ( !indices.empty() && heights[ i ] < heights[ indices.peek() ] ) {
+                   //the index of the rectangle we are trying to analyze
+                   Integer rectangle_i = indices.pop();
+                   //2a. if stack is empty then the effective range of the popped element is i
+                   //    otherwise range is index - peek - 1
+                   if ( indices.empty() )
+                       try_area = heights[ rectangle_i ] * i;
+                   else
+                       try_area = heights[ rectangle_i ] * ( i - indices.peek() - 1 );
+                   if ( try_area > area ) {
+                       area = try_area;
+                       System.out.println( "area set at step i = " + i );
+                   }
+                }
+            }
+            indices.push( i );
+            System.out.println( indices.toString() + " i = " + i );
+        }
+
+        //3. the stack might still contain some stuff (another slice) when we finish the array
+        //   in that case the rectangle spans to the right end of the array
+        System.out.println( indices.toString() );
+        while ( !indices.empty() ) {
+            Integer rectangle_i = indices.pop();
+            if ( indices.empty() )
+                try_area = heights[ rectangle_i ] * ( heights.length );
+            else
+                try_area = heights[ rectangle_i ] * ( heights.length - indices.peek() - 1 );
+            if ( try_area > area ) area = try_area;
+        }
+        return area;
+    }
+
+    public static void main( String[] args ) {
+        largest_hist attempt = new largest_hist();
+        //int[] params = { 2,1,5,6,2,3 };
+        int[] params = { 0,2,1,4,3,2,1,6,1 };
+        //int[] params = { 2,1,2 };
+        //int[] params = { 5,4,4,6,3,2,9,5,4,8,1,0,0,4,7,2 };
+        System.out.println( attempt.solve( params ) );
     }
 
 }
