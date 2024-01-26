0a5ab5d9212352806a10a232ae00a07267a5cd0d
kevinthuhstink <kev72eat@gmail.com>
2024-01-25 23:51:30 -0500

    repo cleanup

--- cool_projects/react/js/export.mjs
+++
@@ -1,9 +0,0 @@
-function preorder( tree ) {
-	let ret = tree.value;
-	if ( tree.left != null )
-		ret += preorder( tree.left );
-	if ( tree.right != null )
-		ret += preorder( tree.right );
-	return ret;
-}
-export { preorder };
