6675509c50f17a3bbe6d35e7dcf265f266584ecb
Kevin Cheng <kev72eat@gmail.com>
2024-03-05 22:54:18 -0500

    change to server-rendered component

--- Q5 - Development/front-end/src/app/page.js
+++ Q5 - Development/front-end/src/app/page.js
@@ -1,29 +1,22 @@
-'use client'
-
 import Topbar from './components/Topbar.js'
 import React from 'react'
 import Dropdown from './components/Dropdown.js'
 
 export default function Page() {
-
-  const [dropdown, setDropdown] = React.useState(false)
-  const mainStyle = "p-4 overflow-auto h-full "
-    + (dropdown ? "w-[calc(100%-200px)]" : "w-full")
-
   return (
     <>
-      <Topbar title="" dropdown={dropdown} setDropdown={setDropdown} />
+      <div className="w-full h-[120px] bg-red-300">
+      </div>
       <main className="flex flex-row h-[calc(100vh-6rem)] w-full fixed bottom-0">
         <div className="flex flex-col w-full">
-          <h1 className="p-4 pt-20 text-9xl bg-red-100">
+          <h1 className="p-4 pt-20 text-9xl bg-red-100 border-b-8 border-black">
             THE<br/>CATALOG.
           </h1>
-          <button className="place-self-center border border-black bg-red-100 rounded
-                             p-3 mt-12">
-            See our items
-          </button>
+          <p className="place-self-center border border-black bg-red-100 rounded
+                        p-3 mt-12">
+            <a href="./catalog">See our items</a>
+          </p>
         </div>
-        <Dropdown showWhen={dropdown} />
       </main>
     </>
   )
