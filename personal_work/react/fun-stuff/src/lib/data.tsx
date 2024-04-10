7ebdba60e9fd3df8bac5ad7dfa49fa465ef06aca
Kevin Cheng <kev72eat@gmail.com>
2024-04-10 19:39:40 -0400

    remove unused images from assets

--- personal_work/react/fun-stuff/src/lib/data.tsx
+++ personal_work/react/fun-stuff/src/lib/data.tsx
@@ -1,8 +1,3 @@
-import leliel from './assets/leliel.webp';
-import kirby from './assets/kirby.png';
-import ramiel from './assets/ramiel.webp';
-import pomni from './assets/pomni.jpg';
-import aperture from './assets/aperture.png'
 import kevinthuhstink from './assets/kevinthuhstink.jpg'
 import jin from './assets/jin.jpg'
 import joon from './assets/joon.jpg'
@@ -12,12 +7,6 @@ import miles from './assets/miles.jpg'
 
 export const data = {
   icons: {
-    default: [
-      leliel,
-      kirby,
-      ramiel,
-      pomni,
-      aperture],
     stinky: [
       kevinthuhstink,
       josh,
