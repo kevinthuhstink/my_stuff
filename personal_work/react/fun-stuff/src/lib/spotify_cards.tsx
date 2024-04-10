4bd5e6460ad8c18a58ae7057223f550c8b586c5f
Kevin Cheng <kev72eat@gmail.com>
2024-04-10 01:19:50 -0400

    add defualt request formats for fetching spotify api

---
+++ personal_work/react/fun-stuff/src/lib/spotify_cards.tsx
@@ -0,0 +1,15 @@
+export const PLAYLIST_REQUEST = {
+  playlist_id: "5RoiU7nEwGT1lHLzz9OuWC",
+  market: "ES",
+  fields: "total",
+}
+
+export const TRACK_REQUEST = {
+  market: "ES"
+}
+
+export const DEFAULT_REQUEST = {
+  playlist_id: "5RoiU7nEwGT1lHLzz9OuWC",
+  market: "ES",
+  fields: "items(track(href,name,id))"
+}
