aef9f1409ef999565dcd6f337e64641cc0af7c13
Kevin Cheng <kev72eat@gmail.com>
2024-05-07 01:20:16 -0400

    successfully fetch playlist tracks

--- personal_work/react/fun-stuff/src/pages/SpotifyCards.tsx
+++ personal_work/react/fun-stuff/src/pages/SpotifyCards.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useState, useContext, useEffect } from "react"
 import { PageLayout } from '@/layout/PageLayout'
 import { Card } from "@/features/spotify_cards/components"
 import { SpotifyAccessToken, getAccessToken, getPlaylistItems, SpotifyTrack } from "@/features/spotify_cards/api"
@@ -8,15 +8,14 @@ import './styles/TodoList.scss'
 export function SpotifyCards() {
 
   const { accessToken, tokenExpiresIn } = useContext(SpotifyAuthContext)
+  const [tracks, setTracks] = useState<SpotifyTrack[]>(null!)
 
   const pageSetup = {
     title: "Spotify Cards",
     sidebarTitle: "2: Reusable Components",
     description: `In the process of making my first interactive page, this was my
                   real first challenge in React. I had to figure out what a prop
-                  was and how to pass them into reusable components. Of course,
-                  now that I'm redoing these pages, I'll step up my game and do a
-                  little more than basic cards.`
+                  was and how to pass them into reusable components.`
   }
 
   useEffect(() => {
@@ -37,10 +36,11 @@ export function SpotifyCards() {
       getAccessToken()
         .then((res: SpotifyAccessToken) => {
           accessToken.current = res.access_token
+          tokenExpiresIn.current = res.expires_in
           console.log(res)
 
           localStorage.setItem("accessToken", res.access_token)
-          localStorage.setItem("accessTokenExpiration", (currentTime + res.expires_in).toString())
+          localStorage.setItem("accessTokenExpiration", `${currentTime + res.expires_in}`)
         })
         .catch((error: Error) => console.log(error.message))
     }
@@ -50,14 +50,15 @@ export function SpotifyCards() {
       console.log(`no action on accessToken\n\n${storedToken}\n\nexpires in ${tokenExpiresIn.current} seconds`)
     }
 
+    getPlaylistItems(accessToken.current)
+      .then((res: SpotifyTrack[]) => {
+        setTracks(res)
+        console.log(res)
+      })
+      .catch((err: Error) => console.log(err.message))
+
     return () => clearInterval(tokenTimerInterval)
   }, [accessToken, tokenExpiresIn])
-
-  useEffect(() => {
-    getPlaylistItems(accessToken.current)
-      .then((res: SpotifyTrack[]) => console.log(res))
-      .catch((error: Error) => console.log(error.message))
-  }, [accessToken])
 
   return (
     <PageLayout {...pageSetup}>
