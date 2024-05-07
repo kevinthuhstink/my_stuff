bd6b958828a36050cb4ab780f5a6c00fd5b4905f
Kevin Cheng <kev72eat@gmail.com>
2024-05-07 19:50:52 -0400

    allow tracks to redirect to their spotify tracks page

--- personal_work/react/fun-stuff/src/features/spotify_cards/components/Card.tsx
+++ personal_work/react/fun-stuff/src/features/spotify_cards/components/Card.tsx
@@ -11,7 +11,7 @@ export function Card({
     const image = album.images[0].url
     const minutes = Math.floor(duration_ms / 60000)
     const seconds = Math.floor(duration_ms % 60000 / 1000)
-    const time = `${minutes}:${seconds}`
+    const time = `${minutes}:${seconds >= 10 ? seconds : "0" + seconds}`
 
     return (
         <div className="card-wrapper">
@@ -19,10 +19,14 @@ export function Card({
                 <h1 className="track-name">{name}</h1>
                 <h1 className="track-time">{time}</h1>
             </header>
-            <div className="card" style={{ backgroundImage: `url(${image})` }}>
-                { explicit && <h1 className="explicit">EXPLICIT</h1> }
-                {id}
-            </div>
+            <a href={"https://open.spotify.com/track/" + id}>
+                <div
+                    className="card"
+                    style={{ backgroundImage: `url(${image})` }}
+                >
+                    { explicit && <h1 className="explicit">EXPLICIT</h1> }
+                </div>
+            </a>
         </div>
     )
 }
