c7c3322cfdc81e2ac3cd6c1f4a0fd6e3403ca97b
Kevin Cheng <kev72eat@gmail.com>
2024-05-07 18:42:22 -0400

    change main div width if sidebar is hidden or not

--- personal_work/react/fun-stuff/src/layout/PageLayout.tsx
+++ personal_work/react/fun-stuff/src/layout/PageLayout.tsx
@@ -1,5 +1,7 @@
+import { useContext } from "react"
 import { Navbar } from '@/components/Navbar'
 import { Sidebar } from '@/components/Sidebar'
+import { DisplayContext } from "@/contexts/DisplayContext"
 
 interface PageLayoutProps {
   bare?: boolean,
@@ -16,13 +18,16 @@ export function PageLayout({
   description,
   children
 }: PageLayoutProps) {
+  const { display } = useContext(DisplayContext)
+  const { hideSidebar } = display
+
   return (
     <div>
       { !bare ?
         <>
           <Navbar title={title} />
           <div id="under-nav">
-            <main id="main">
+            <main id="main" style={hideSidebar ? { width: "100%" } : {}}>
               {children}
             </main>
             <Sidebar title={sidebarTitle} description={description} />
